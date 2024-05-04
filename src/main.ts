c23c7bd343619c5c20743380bf1b6325a58b41a5
Yan Soares da Silva <yansoaressilva@outlook.com>
2024-05-04 10:46:36 -0300

    feat: setup oh-vue-icons

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import './assets/base.css';
 
 import { createApp } from 'vue';
+import { OhVueIcon, addIcons } from 'oh-vue-icons';
+import { BiCheckCircleFill, BiExclamationLg, BiExclamationTriangleFill } from 'oh-vue-icons/icons';
 import App from './App.vue';
 
+addIcons(BiCheckCircleFill, BiExclamationLg, BiExclamationTriangleFill);
+
 const app = createApp(App);
+app.component('v-icon', OhVueIcon);
 
 app.mount('#app');
