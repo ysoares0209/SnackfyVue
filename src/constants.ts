13f49366e04be2890cd7d5b8b7bf77ded65247aa
Yan Soares da Silva <yansoaressilva@outlook.com>
2024-05-04 11:01:15 -0300

    feat: add INFO_COLOR & INFO_ICON to constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,9 @@
 export const SUCCESS_COLOR = '#63b365';
+export const INFO_COLOR = '#5eafe6';
 export const ERROR_COLOR = '#f22e2e';
 
 export const SUCCESS_ICON = 'bi-check-circle-fill';
+export const INFO_ICON = 'bi-exclamation-lg';
 export const ERROR_ICON = 'bi-exclamation-triangle-fill';
 
 export const DEFAULT_DURATION = 2.5;
