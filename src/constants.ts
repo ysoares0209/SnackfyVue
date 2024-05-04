65e25b9c28bbf70ea84a4ae3adf4a3111ee05800
Yan Soares da Silva <yansoaressilva@outlook.com>
2024-05-04 11:15:54 -0300

    feat: update info icon

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ export const INFO_COLOR = '#5eafe6';
 export const ERROR_COLOR = '#f22e2e';
 
 export const SUCCESS_ICON = 'bi-check-circle-fill';
-export const INFO_ICON = 'bi-exclamation-lg';
+export const INFO_ICON = 'bi-exclamation-circle-fill';
 export const ERROR_ICON = 'bi-exclamation-triangle-fill';
 
 export const DEFAULT_DURATION = 2.5;
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import './assets/base.css';
 
 import { createApp } from 'vue';
 import { OhVueIcon, addIcons } from 'oh-vue-icons';
-import { BiCheckCircleFill, BiExclamationLg, BiExclamationTriangleFill } from 'oh-vue-icons/icons';
+import {
+  BiCheckCircleFill,
+  BiExclamationCircleFill,
+  BiExclamationTriangleFill
+} from 'oh-vue-icons/icons';
 import App from './App.vue';
 
-addIcons(BiCheckCircleFill, BiExclamationLg, BiExclamationTriangleFill);
+addIcons(BiCheckCircleFill, BiExclamationCircleFill, BiExclamationTriangleFill);
 
 const app = createApp(App);
 app.component('v-icon', OhVueIcon);
