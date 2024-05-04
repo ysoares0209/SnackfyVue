845213753f6f67b8f56d34b58019f4ae976c416f
Yan Soares da Silva <yansoaressilva@outlook.com>
2024-05-04 10:49:26 -0300

    feat: add icon names constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,9 @@
 export const SUCCESS_COLOR = '#63b365';
 export const ERROR_COLOR = '#f22e2e';
 
+export const SUCCESS_ICON = 'bi-check-circle-fill';
+export const ERROR_ICON = 'bi-exclamation-triangle-fill';
+
 export const DEFAULT_DURATION = 2.5;
 // offset to ensure the notification is removed after the animation plays out
 export const DURATION_OFFSET_IN_MS = 300;
